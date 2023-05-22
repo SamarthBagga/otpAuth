73ae170ccb5a5ba59f44fd76d6049469548667a3
Samarth Bagga <samarthbagga@gmail.com>
2023-05-22 22:01:16 +0530

    redux-added

--- client/src/screens/LoginScreen.jsx
+++ client/src/screens/LoginScreen.jsx
@@ -1,13 +1,24 @@
 import React from 'react'
-import { useState } from 'react'
-import {Link} from 'react-router-dom';
-import { Form,Button, Row, Col } from 'react-bootstrap';
+import { useState, useEffect } from 'react'
+import { Link, useNavigate } from 'react-router-dom';
+import { Form, Button, Row, Col } from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux'
 import FormContainer from '../components/FormContainer';
-import { set } from 'mongoose';
+import { useLoginMutation } from '../slices/usersApiSlice';
+import { setCredentials } from '../slices/authSlice';
 
 const LoginScreen = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+
+    const [login, { isLoading }] = useLoginMutation();
+
+    const { userInfo } = useSelector((state)=> state.)
+
+
     
     const submitHandler = async (e) =>{
         e.preventDefault();
